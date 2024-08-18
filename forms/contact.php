f18953f74d09033429eddf5d5e0f2ae83dc709aa
miladsoft <miladsoft@users.noreply.github.com>
2024-08-18 11:08:15 +0000

    deploy: 816bcbc10222bded9b5deeeb674236c4a41ab44e

--- forms/contact.php
+++ forms/contact.php
@@ -6,8 +6,8 @@
   * For more info and help: https://sbc.om/php-email-form/
   */
 
-  // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'contact@example.com';
+  // Replace contact@creativeccs.com with your real receiving email address
+  $receiving_email_address = 'contact@creativeccs.com';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
@@ -26,7 +26,7 @@
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
   /*
   $contact->smtp = array(
-    'host' => 'example.com',
+    'host' => 'creativeccs.com',
     'username' => 'example',
     'password' => 'pass',
     'port' => '587'
